40de9bfdc3775a87e78b7fd32d2b56f964f4d39f
LChanwoo <chw210601@gmail.com>
2022-04-27 13:45:53 +0900

    2022-04-27 -2

--- myboot1-test/src/main/java/edu/multi/boot1/MyWebConfig.java
+++ myboot1-test/src/main/java/edu/multi/boot1/MyWebConfig.java
@@ -8,14 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry
-		.addResourceHandler("/upload/**")
-		.addResourceLocations("file:///c:/upload/");
-		
-		
-	}
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry
+//		.addResourceHandler("/upload/**")
+//		.addResourceLocations("file:///upload/");
+//		
+//		
+//	}
 	
 //1024byte -> 1kb 
 //	1024kb -> 1mb
